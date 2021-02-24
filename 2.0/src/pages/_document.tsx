0fee3ee16bf4b0698678387b944a147a47f80b1a
allyson <allyson.brandao@venhapranuvem.com.br>
2021-02-23 23:40:40 -0300

    Alterado titulo

--- 2.0/src/pages/_document.tsx
+++ 2.0/src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document{
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"/>
                     <link rel="shortcut icon" href="./favicon.png"/>
-                    <title>FocusTimer</title>
+                    <title>Inicio | FocusTimer</title>
                 </Head>
                 <body>
                     <Main/>
